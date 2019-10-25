8888a76062916a827d9f2be261310f308697f187
qskane <qskane@qq.com>
2019-10-25 17:31:00 +0800

    update

--- x.php
+++ x.php
@@ -1,26 +1,6 @@
 <?php
 
-function getFibonacci()
-{
-    $i = 0;
-    $k = 1; //first fibonacci value
-    yield $k;
-    while(true)
-    {
-        $k = $i + $k;
-        $i = $k - $i;
-        yield $k;
-    }
-}
-
-$y = 0;
-
-foreach(getFibonacci() as $fibonacci)
-{
-    echo $fibonacci . "\n";
-    $y++;
-    if($y > 10)
-    {
-        break; // infinite loop prevent
-    }
-}
+$array1 = [0 => 'zero_a', 2 => 'two_a', 3 => 'three_a'];
+$array2 = [1 => 'one_b', 3 => 'three_b', 4 => 'four_b'];
+//$result = $array1 + $array2;
+var_export(array_merge($array1, $array2));
