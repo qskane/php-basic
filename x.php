9d2f70954d5fa93dca128d0131f0c3749946f9fd
qskane <qskane@qq.com>
2019-10-29 17:26:31 +0800

    update

--- x.php
+++ x.php
@@ -1,6 +1,4 @@
 <?php
+$fruits = ["d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple"];
 
-$array1 = [0 => 'zero_a', 2 => 'two_a', 3 => 'three_a'];
-$array2 = [1 => 'one_b', 3 => 'three_b', 4 => 'four_b'];
-//$result = $array1 + $array2;
-var_export(array_merge($array1, $array2));
+var_dump($fruits);
